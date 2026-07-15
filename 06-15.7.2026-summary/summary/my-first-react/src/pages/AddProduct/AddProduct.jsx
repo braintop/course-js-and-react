3480b76750c69eb541f93fe1579ab22de43e6738
braintop <braintop7@gmail.com>
2026-07-15 09:45:18 +0300

    add

--- 06-15.7.2026-summary/summary/my-first-react/src/pages/AddProduct/AddProduct.jsx
+++ 06-15.7.2026-summary/summary/my-first-react/src/pages/AddProduct/AddProduct.jsx
@@ -1,7 +1,33 @@
 export default function AddProduct() {
+    const [product, setProduct] = useState({
+        name: '',
+        price: 0,
+        image: '',
+        inStock: false
+    })
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(product)
+
+    }
+
+    function handleChange(e) {
+        setProduct({
+             ...product,
+             [e.target.name]: e.target.value
+             }
+        )
+    }
     return (
         <div>
             <h1>Add Product</h1>
+            <form onSubmit={handleSubmit}>
+                <input name="name" onChange={handleChange} type="text" placeholder="Product Name" />
+                <input name="price" onChange={handleChange}  type="number" placeholder="Product Price" />
+                <input name="image" onChange={handleChange}  type="text" placeholder="inage url" />
+                <input name="inStock" onChange={handleChange}  type="checkbox" checked={product.inStock} />
+                <button type="submit">Add Product</button>
+            </form>
         </div>
     )
 }
