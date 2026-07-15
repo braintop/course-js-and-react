48773e7b9d9b3c7b313bf9418cb926f7fc5ccd4b
braintop <braintop7@gmail.com>
2026-07-15 08:30:18 +0300

    Add summary

---
+++ 06-15.7.2026-summary/summary/my-first-react/src/data/products.js
@@ -0,0 +1,16 @@
+export let products = [
+    {
+        id: 1,
+        name: 'Product 1',
+        price: 100,
+        image: 'https://via.placeholder.com/150',
+        instock: true
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        price: 200,
+        image: 'https://via.placeholder.com/150',
+        instock: false
+    }
+]
